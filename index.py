4781d4aad2b02770de01807ee05ff7bd0e706cf6
HyunsooKim <hyunsoo.ki,m@li-st.com>
2023-04-14 08:08:49 +0900

    test_2

--- index.py
+++ index.py
@@ -5,8 +5,7 @@ import os, streamlit as st
 
 
 from pathlib import Path
-from gpt_index import download_loader
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper
+from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, download_loader
 from langchain import OpenAI
 
 # This example uses text-davinci-003 by default; feel free to change if desired
